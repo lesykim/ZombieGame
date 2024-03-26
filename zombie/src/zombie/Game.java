5bb958657571414d288a94530f9e5552e0ce0a0b
lesykim <trchol01@naver.com>
2024-03-26 15:44:03 +0900

    Add Game

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -1,11 +1,18 @@
 package zombie;
 
 public class Game {
+	private Zombie zombie;
+	private Hero hero;
+	private Boss boss;
+	
 	private Game() {
-		
+		zombie = new Zombie(5,100,5);
+		hero = new Hero(0,200,10);
+		boss = new Boss(10,300,20,100);
 	}
 	
 	private static Game instance = new Game();
+	
 	public static Game getInstance() {
 		return instance;
 	}

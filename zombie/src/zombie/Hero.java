2d40b4823b757dc52fd3c37296e776c147d04cea
lesykim <trchol01@naver.com>
2024-03-26 15:48:59 +0900

    Update attack()

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -9,6 +9,9 @@ public class Hero extends Unit implements Damage, Attack{
 	@Override
 	public void demage(int attack) {
 		while(attack>0) {
+			if(this.isDead()) {
+				break;
+			}
 			this.setHpMinus();
 			attack--;
 		}
@@ -16,7 +19,20 @@ public class Hero extends Unit implements Damage, Attack{
 
 	@Override
 	public void attack(Unit monster) {
-		
+		int attack = ran.nextInt(this.getMax())+2;
+		int random = ran.nextInt(5);
+		if(random == 1) {
+			attack *= 2;
+			System.out.println("크리티컬💥💥 ");
+		}
+		System.out.printf("Hero %d 공격⚔️\n",attack);
+		while(attack>0) {
+			if(monster.isDead()) {
+				break;
+			}
+			monster.setHpMinus();
+			attack--;
+		}
 	}
 	
 }
